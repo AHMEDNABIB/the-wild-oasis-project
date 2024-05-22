ddf975a5ef85847f81007f72ae62817b25328a4e
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-22 13:02:18 +0600

    complete the home page

---
+++ app/account/page.js
@@ -0,0 +1,8 @@
+
+export const metadata = {
+	title: "Guest Area",
+};
+
+export default function Page() {
+  return <h1>Your accohgunt</h1>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Cabins'
+}
+
+
+export default async function Page() {
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      
+    </div>
+  );
+}
