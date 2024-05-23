0190aeaa8ffbc12a2f1f138b353814d6af6e9d59
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-23 14:25:10 +0600

    add suspension to cabin page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,15 @@
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
-  title: 'Cabins'
-}
-
+	title: "Cabins",
+};
 
 export default async function Page() {
-
-	const cabins = await getCabins()
-	
 	// console.log(cabins)
 
-   return (
+	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
 				Our Luxury Cabins
@@ -26,13 +23,9 @@ export default async function Page() {
 				perfect spot for a peaceful, calm vacation. Welcome to paradise.
 			</p>
 
-			{cabins.length > 0 && (
-				<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-					{cabins.map((cabin) => (
-						<CabinCard cabin={cabin} key={cabin.id} />
-					))}
-				</div>
-			)}
+			<Suspense fallback={<Spinner/>}>
+				<CabinList />
+			</Suspense>
 		</div>
-   );
+	);
 }
