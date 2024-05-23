e6e07caedded6200fdab66aa12f423305e06d9d6
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-23 17:07:02 +0600

    error and page-found page is added

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -19,11 +19,12 @@ export default async function Page() {
 				Italian Dolomites. Imagine waking up to beautiful mountain
 				views, spending your days exploring the dark forests around, or
 				just relaxing in your private hot tub under the stars. Enjoy
-				nature's beauty in your own little home away from home. The
-				perfect spot for a peaceful, calm vacation. Welcome to paradise.
+				nature`&apos;`s beauty in your own little home away from home.
+				The perfect spot for a peaceful, calm vacation. Welcome to
+				paradise.
 			</p>
 
-			<Suspense fallback={<Spinner/>}>
+			<Suspense fallback={<Spinner />}>
 				<CabinList />
 			</Suspense>
 		</div>
