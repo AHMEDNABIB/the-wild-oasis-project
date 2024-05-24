6cecaac8e51d3f63cffc1f94a9184fa061de28da
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-24 17:12:32 +0600

    add filter functionality in cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 0 
 
@@ -8,8 +9,10 @@ export const metadata = {
 	title: "Cabins",
 };
 
-export default async function Page() {
-	// console.log(cabins)
+export default async function Page({searchParams}) {
+	console.log(searchParams)
+
+	const filter = searchParams?.capacity ?? "all"
 
 	return (
 		<div>
@@ -26,8 +29,14 @@ export default async function Page() {
 				paradise.
 			</p>
 
-			<Suspense fallback={<Spinner />}>
-				<CabinList />
+			<div className =" flex justify-end mb-8">
+			<Filter/>
+
+			</div>
+
+
+			<Suspense fallback={<Spinner />} key={filter}>
+				<CabinList filter={filter} />
 			</Suspense>
 		</div>
 	);
