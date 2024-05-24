63d340bd705c969389e0cb3caa2ea35cc16f1fb6
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-24 19:32:34 +0600

    add functionality to select reservation date

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
 import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 const josefin = Josefin_Sans({
 	subsets: ["latin"],
@@ -25,11 +26,12 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body
 				className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
-				<Header/>
+				<Header />
 				<div className="flex-1 px-8 py-12 grid">
-					<main className="max-w-7xl mx-auto w-full">{children}</main>
+					<main className="max-w-7xl mx-auto w-full">
+						<ReservationProvider>{children}</ReservationProvider>
+					</main>
 				</div>
-				
 			</body>
 		</html>
 	);
