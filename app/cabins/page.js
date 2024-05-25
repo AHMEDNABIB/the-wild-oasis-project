77673c8e8cea231ccb7ab4d220c956888f8bf2d8
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-25 20:01:39 +0600

    add reservation reminder

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -38,7 +38,7 @@ export default async function Page({searchParams}) {
 
 			<Suspense fallback={<Spinner />} key={filter}>
 				<CabinList filter={filter} />
-				<ReservationReminder/>
+				<ReservationReminder />
 			</Suspense>
 		</div>
 	);
